4be53ff90734d4785e72dd2227f0b627c0f55719
sajker <sajkerprog@gmail.com>
2019-06-29 13:34:53 +0300

    todo example

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto accusantium ratione velit enim nihil beatae facere commodi vitae alias quisquam.</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
